8e6d1588ccdded5ff1635346dd487b7b956bc24c
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-07-21 01:33:29 +0530

    fix icon size

--- components/cta.js
+++ components/cta.js
@@ -15,19 +15,19 @@ export const Cta = () => {
         </div>
         <div className='grid gap-10 grid-cols-2 md:grid-cols-4 px-5'>
             <div className='text-center flex flex-col gap-4 items-center'>
-                <FaWpforms size={70} />
+                <FaWpforms size={30} />
                 <p className='text-base'>1. Fill & application</p>
             </div>
             <div className='text-center flex flex-col gap-4 items-center'>
-            <FaMagnifyingGlass size={70} />
+            <FaMagnifyingGlass size={30} />
                 <p className='text-base'>2. Designers at Iraada review your profile and check for alignment with our program</p>
             </div>
             <div className='text-center flex flex-col gap-4 items-center'>
-            <FaTicket size={70} />
+            <FaTicket size={30} />
                 <p className='text-base'>3. If accepted, you are offered a seat at the Iraada festival</p>
             </div>
             <div className='text-center flex flex-col gap-4 items-center'>
-            <FaWallet size={70} />
+            <FaWallet size={30} />
                 <p className='text-base'>4. You pay in 24 hours to reserve your seat.</p>
             </div>
         </div>
