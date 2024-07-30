bd782a7cb8a5260f3bb6fb12e9a9ce7e70b1d6b8
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-07-31 01:23:31 +0530

    improve stats

--- components/stat2.js
+++ components/stat2.js
@@ -9,21 +9,33 @@ export const Stat2 = () => {
   return (
     <div className='py-12'>
         <div className='container mx-auto'>
-        <div className='grid gap-2 grid-cols-3 px-5'>
-            <div className='text-center'>
-                <h3 className='text-5xl font-bold text-[#a8ca8f]'><CountUp enableScrollSpy start={0} end={8} />+</h3>
-                <h4 className='text-2xl font-bold text-[#a8ca8f] font-seasons'>Workshops</h4>
-                <p>Introspection, Design, Action Plan</p>
+        <div className='grid gap-2 grid-cols-1 md:grid-cols-3 px-5 items-start'>
+            <div className='text-center flex flex-col gap-2 justify-center items-center mb-5'>
+                <div className='text-5xl font-bold text-[#a8ca8f] bg-[#E268F2] w-24 h-24 rounded-full flex items-center justify-center'>
+                  <div className='text-[#ffffff]'>
+                  <CountUp enableScrollSpy start={0} end={8} />+
+                  </div>
+                </div>
+                <h4 className='text-2xl font-bold text-[#216A5A] font-seasons'>Workshops</h4>
+                <p className='text-[#a8ca8f]'>Introspection, Design, Action Plan</p>
             </div>
-            <div className='text-center'>
-                <h3 className='text-5xl font-bold text-[#a8ca8f]'><CountUp enableScrollSpy start={0} end={4} />+</h3>
-                <h4 className='text-2xl font-bold text-[#a8ca8f] font-seasons'>Spots</h4>
-                <p>to pause, reflect, play and laugh (at oneself also)</p>
+            <div className='text-center flex flex-col gap-2 justify-center items-center mb-5'>
+              <div className='text-5xl font-bold text-[#a8ca8f] bg-[#E268F2] w-24 h-24 rounded-full flex items-center justify-center'>
+                  <div className='text-[#ffffff]'>
+                  <CountUp enableScrollSpy start={0} end={4} />+
+                  </div>
+                </div>
+                <h4 className='text-2xl font-bold text-[#216A5A] font-seasons'>Spots</h4>
+                <p className='text-[#a8ca8f]'>to pause, reflect, play and laugh (at oneself also)</p>
             </div>
-            <div className='text-center'>
-                <h3 className='text-5xl font-bold text-[#a8ca8f]'>1</h3>
-                <h4 className='text-2xl font-bold text-[#a8ca8f] font-seasons'>Community</h4>
-                <p>of growth & support</p>
+            <div className='text-center flex flex-col gap-2 justify-center items-center mb-5'>
+            <div className='text-5xl font-bold text-[#a8ca8f] bg-[#E268F2] w-24 h-24 rounded-full flex items-center justify-center'>
+                  <div className='text-[#ffffff]'>
+                  1
+                  </div>
+                </div>
+                <h4 className='text-2xl font-bold text-[#216A5A] font-seasons'>Community</h4>
+                <p className='text-[#a8ca8f]'>of growth & support</p>
             </div>
         </div>
       </div>
