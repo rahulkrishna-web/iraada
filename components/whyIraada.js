70d9eb31d000a4f05bc1022b90c774afbc320ad0
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-07-31 19:32:18 +0530

    improve why iraada

--- components/whyIraada.js
+++ components/whyIraada.js
@@ -5,32 +5,32 @@ import { FaLightbulb, FaMapMarkerAlt, FaUsers } from 'react-icons/fa';
 
 export const WhyIraada = () => {
   return (
-    <div className='py-24 bg-[#F7FAF8]'>
+    <div className='py-24 bg-[#3c8572] text-white'>
         <div className='container mx-auto'>
         <h2 className='text-3xl font-bold text-center mb-4 font-seasons'>Why Iraada is for you?</h2>
         <p className='px-5 md:max-w-2xl	md:mx-auto'>
         Struggling to juggle multiple roles—
 creator, entrepreneur, side hustler,
 or explorer—and still seeking your
-true path?
+true path?<br /><br />
 
 
 
 Is your creative energy craving a
 new direction or a vibrant stage to
-truly shine?
+truly shine?<br /><br />
 
 
 
 Excited to meet inspiring people but
 unsure how to spark those
-meaningful connections?
+meaningful connections?<br /><br />
 
 
 
 Tired of conferences that
 overpromise but underdeliver on
-real, impactful experiences?
+real, impactful experiences?<br /><br />
 
 
 
