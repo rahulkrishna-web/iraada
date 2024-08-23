8e1bd846967a4a44d7925e18d203524b4297fad8
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-08-23 14:45:08 +0530

    add icons

--- components/lead-video.js
+++ components/lead-video.js
@@ -4,6 +4,11 @@ import { Button, Image } from "@nextui-org/react";
 export const LeadVideo = () =>{
   return(
     <section className="w-full">
+      <div className="icons-bar">
+        <div>Icon 1</div>
+        <div>Icon 2</div>
+        <div>Icon 3</div>
+      </div>
       <Image className="hidden md:block w-full" radius="none" width={1366} alt="Iraada" src="/banner-desktop.webp" />
       <Image className="md:hidden" radius="none" width={600} alt="Iraada" src="/banner-mobile.webp" />
     </section>
--- components/lead.js
+++ components/lead.js
@@ -11,7 +11,14 @@ import { Button, Image } from "@nextui-org/react";
 export const Lead = () =>{
   return(
     <section className="">
-      <div>
+      <div className="relative">
+      <div className="px-2 py-2 w-full bg-white">
+        <div className="flex justify-between items-center">
+        <div><Image width={100} radius="none" alt="Iraada" src="/jito_chennai.png" /></div>
+        <div><Image width={80} radius="none" alt="Iraada" src="/jito.png" /></div>
+        <div><Image width={100} radius="none" alt="Iraada" src="/jito_youth.png" /></div>
+        </div>
+      </div>
       <Image fill className="w-screen md:hidden" radius="none" alt="Iraada" src="/banner-mobile.gif" />
       <Image fill className="w-screen hidden md:block" radius="none" alt="Iraada" src="/banner-desktop.gif" />
       </div>
