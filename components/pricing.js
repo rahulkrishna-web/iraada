c9642f227f47e092538e3d83601e975f14dbbed1
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-08-27 11:58:51 +0530

    change pricing

--- components/pricing.js
+++ components/pricing.js
@@ -9,7 +9,7 @@ export const Pricing = () => {
         <div className='flex flex-col items-center justify-center	 mx-auto h-dvh'>
         <h2 className='text-3xl py-3 font-bold text-center mb-4 font-seasons bg-[#f7faf8] w-full text-[#3d8473]'>Paisa Kitna Lagenga?</h2>
         <p className='text-lg md:text-2xl px-5 md:max-w-2xl	md:mx-auto text-white text-center'>
-        Access to all transformative workshops across 3 days is at ₹ 15,999/- towards design of the event, curation of the workshops, hospitality for facilitators and speakers
+        Access to all transformative workshops across 3 days is at ₹ 9,999/- towards design of the event, curation of the workshops, hospitality for facilitators and speakers
 
         </p>
       </div>
