246aab8cfe61a232e8e4bdb576d340b93ca34767
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-08-30 10:30:13 +0530

    fix section going left in high res desktop

--- components/sensoryExplain.js
+++ components/sensoryExplain.js
@@ -6,7 +6,7 @@ import { FaLightbulb, FaMapMarkerAlt, FaUsers } from 'react-icons/fa';
 export const SensoryExplain = () => {
   return (
     <div className="bg-[url('/iraada-bg.png')] text-[#3c8572] px-2">
-        <div className='container flex items-center	justify-items-center h-dvh'>
+        <div className='container flex items-center	justify-items-center h-dvh w-full mx-auto'>
         <p className='text-2xl px-5 md:max-w-lg	md:mx-auto text-center bg-white py-14 px-14'>
         The sensory exploration is designed to spark <br />
         <span className="text-[#e368f4] font-bold">Creative problem solving</span><br />
